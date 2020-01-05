919549e1387b4cb507db7bb4d538ffef06d2b4e4
IvanLuLyf <835498692@qq.com>
2020-01-06 02:46:11 +0800

    v11 bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v10';
+const CATCH_VER = 'v11';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
