7543d5894ed313dd6ee2819c8474d22e9bfbd159
IvanLuLyf <835498692@qq.com>
2020-01-05 18:17:07 +0800

    v8 change translate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v7';
+const CATCH_VER = 'v8';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
