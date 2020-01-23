8570bc896cc21151624092982ea68ff415a85c44
IvanLuLyf <835498692@qq.com>
2020-01-24 02:22:05 +0800

    v19 Video Size

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CATCH_VER = 'v18';
+const CATCH_VER = 'v19';
 self.addEventListener('install', function (event) {
     let languages = ['zh-cn', 'en-us'];
     let languageCode = (navigator.language || 'zh-cn').toLocaleLowerCase();
